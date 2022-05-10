f93c6ae761db53bec144875d17aa73800f0e131f
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-05-09 22:07:40 -0300

    concluindo parte 2 do desafio. Validações.

--- ProjetoCalcWebAPI.Web/Controllers/PrimeiraCalcComAPI.cs
+++ ProjetoCalcWebAPI.Web/Controllers/PrimeiraCalcComAPI.cs
@@ -10,24 +10,48 @@ namespace ProjetoCalcWebAPI.Web.Controllers
         [HttpGet("Soma")]
         public IActionResult SomaAPI(int primeiroNum, int segundoNum)
         {
+            if (primeiroNum == 0 || primeiroNum > 999 || segundoNum == 0 || segundoNum > 999)
+            {
+                return BadRequest("Parâmetro(s) inválido(s), nenhum dos parâmetros pode ser igual a 0 ou maior que 999.");
+            }
             var resultado = primeiroNum + segundoNum;
             return Ok($"O resultado da soma é: {resultado}");
         }
         [HttpGet("Subtração")]
         public IActionResult SubtracaoAPI(int primeiroNum, int segundoNum)
         {
+            if (primeiroNum == 0 || primeiroNum > 999 || segundoNum == 0 || segundoNum > 999)
+            {
+                return BadRequest("Parâmetro(s) inválido(s), nenhum dos parâmetros pode ser igual a 0 ou maior que 999.");
+            }
             var resultado = primeiroNum - segundoNum;
             return Ok($"O resultado da subtração é: {resultado}");
         }
         [HttpGet("Multiplicação")]
         public IActionResult MultiplicacaoAPI(int primeiroNum, int segundoNum)
         {
+            if (primeiroNum == 0 || primeiroNum > 999 || segundoNum == 0 || segundoNum > 999)
+            {
+                return BadRequest("Parâmetro(s) inválido(s), nenhum dos parâmetros pode ser igual a 0 ou maior que 999.");
+            }
+            if (primeiroNum < 0 || segundoNum < 0)
+            {
+                return BadRequest("Parâmetro inválido, nenhum dos parâmetros pode ser menor que 0.");
+            }
             var resultado = primeiroNum * segundoNum;
             return Ok($"O resultado da multiplicação é: {resultado}");
         }
         [HttpGet("Divisão")]
         public IActionResult DivisaoAPI(int primeiroNum, int segundoNum)
         {
+            if (primeiroNum == 0 || primeiroNum > 999 || segundoNum == 0 || segundoNum > 999)
+            {
+                return BadRequest("Parâmetro(s) inválido(s), nenhum dos parâmetros pode ser igual a 0 ou maior que 999.");
+            }
+            if (primeiroNum < 0 || segundoNum < 0)
+            {
+                return BadRequest("Parâmetro inválido, nenhum dos parâmetros pode ser menor que 0.");
+            }
             var resultado = primeiroNum / segundoNum;
             return Ok($"O resultado da divisão é: {resultado}");
         }
